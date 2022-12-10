64b1b26a623d1648d5ea8b3b450dbce413f54aa5
harrison <hwhite9@wisc.edu>
2022-12-10 15:29:30 -0600

    continued insert implementation

--- Stage6/insert.C
+++ Stage6/insert.C
@@ -14,14 +14,50 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
-	// Ensure no nulls are present before we try to insert
+	// make sure attrCount corresponds to # of attr in relation
+	RelDesc currRelation;
+	Status status = relCat->getInfo(relation, currRelation);
+	if (status != OK) {
+		return status;
+	}
+	if (currRelation.attrCnt != attrCnt) {
+		return OK;
+	}
+
+	// Ensure no nulls are present before we try to insert, and figure out the total size of the record to insert
+	int recSize = 0;
 	for (int i = 0; i < attrCnt; i++) {
 		if (attrList[i].attrValue == NULL) {
 			return ATTRNOTFOUND;
 		}
+		recSize += attrList[i].attrLen;
+	}
+	
+	// create Record to hold all the data in the proper order
+	Record recToInsert;
+	recToInsert.length = recSize;
+	void * recData = malloc(recSize);
+	recToInsert.data = recData;
+	
+	// fetch the data and put it in recData
+	for (int i = 0; i < attrCnt; i++) {
+		AttrDesc currAttrDesc;
+		status = attrCat->getInfo(relation, attrList[i].attrName, currAttrDesc);
+		if (status != OK) {
+			return status;
+		}
+
+		memcpy((recData + currAttrDesc.attrOffset), attrList[i].attrValue, attrList[i].attrLen); // TODO figure out proper way to increment the recData pointer
 	}
 
-	// 
+	// insert into heapfile for the relation, which is done by creating a new InsertFileScan object
+	InsertFileScan *ifs = new InsertFileScan(relation, status);
+	RID recRID;
+	ifs->insertRecord(recToInsert, recRID);
+
+	// deallocate mem
+	free(recData);
+	delete ifs;
 	return OK;
 }
 
