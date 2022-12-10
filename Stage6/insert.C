e8863cdfa0e035ccb200d3fb9f76615f4f65cc3b
harrison <hwhite9@wisc.edu>
2022-12-10 17:19:53 -0600

    finished implementing QU_insert()

--- Stage6/insert.C
+++ Stage6/insert.C
@@ -36,7 +36,7 @@ const Status QU_Insert(const string & relation,
 	// create Record to hold all the data in the proper order
 	Record recToInsert;
 	recToInsert.length = recSize;
-	void * recData = malloc(recSize);
+	void *recData = malloc(recSize);
 	recToInsert.data = recData;
 	
 	// fetch the data and put it in recData
@@ -44,16 +44,23 @@ const Status QU_Insert(const string & relation,
 		AttrDesc currAttrDesc;
 		status = attrCat->getInfo(relation, attrList[i].attrName, currAttrDesc);
 		if (status != OK) {
+			free(recData);
 			return status;
 		}
-
-		memcpy((recData + currAttrDesc.attrOffset), attrList[i].attrValue, attrList[i].attrLen); // TODO figure out proper way to increment the recData pointer
+		// copy data into appropiate spot in recData
+		memcpy((recData + currAttrDesc.attrOffset), attrList[i].attrValue, attrList[i].attrLen); // TODO is this the proper way to incorporate the offset for recData?
 	}
 
 	// insert into heapfile for the relation, which is done by creating a new InsertFileScan object
+	// TODO unsure on if it's supposed to create a new object here... that's the only way I can think of to do it, b/c I can't find any already-created heapfile object for the relation
 	InsertFileScan *ifs = new InsertFileScan(relation, status);
 	RID recRID;
-	ifs->insertRecord(recToInsert, recRID);
+	status = ifs->insertRecord(recToInsert, recRID);
+	if (status != OK) {
+		free(recData);
+		delete ifs;
+		return status;
+	}
 
 	// deallocate mem
 	free(recData);
